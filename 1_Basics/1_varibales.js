8b6febbb7a01ef765a0b6c36dbd4afabb51a66d6
ribalraza1 <ribalraza1@gmail.com>
2025-07-21 13:54:29 +0000

    variables in js and different ways to initialize/declare it

--- 1_Basics/1_varibales.js
+++ 1_Basics/1_varibales.js
@@ -1,31 +1,28 @@
 /**
- * Declares variables using different declaration keywords and highlights best practices.
- *
- * @type {number}
- * @const
- * @description A unique identifier, typically representing a user or entity.
+ * Variables are like labeled boxes in programming that hold different types of information.
+ * They give names to data, making it easy to use and change that data throughout the code.
  */
-const id = 12223;
-// id = 23335; // This assignment is invalid because `id` is declared as `const`.
-/**
- * @type {string}
- * @description The user's email address.
- */
-let email = "raza@yahoo.net";
+// there are two main ways to declare a variable in JavaScript: 'const' and 'let'.
+// 1. With 'const'
+// After defining a variable with const, it's value can't be changed in program later.
+const valueOfPi = 3.14;
+// valueOfPi = 3.24; // ❌ 'TypeError: Assignment to constant variable.'
 
-/**
- * @type {string}
- * @deprecated Avoid using `var` due to potential scoping issues. Use `let` or `const` instead.
- */
-var password = "";
+// 2. With 'let'
+// Variables declared with 'let' can be reassigned/changed later in the program.
+// It allows for flexibility when the value of a variable needs to change.
+let nameOfPerson = "Azam";
+nameOfPerson = "Azam Ahmed"; // ✅
 
-/**
- * @type {string}
- * @description The user's city of residence.
- * @example city = "Karachi"; // Example of valid assignment
- * @example city = 25; // Example of invalid assignment (type mismatch)
- * @example city; // Example of accessing the city value
- */
-let city = "Multan";
+// There is an older way of declaring variable with 'var' keyword.
+// But it is not recommended
+var ageOfPerson = 21;
+ageOfPerson = 22;
 
+// Comparison of let vs var
+// 'let' was introduced to solve the issues with 'var' regarding scope and hoisting.
+// It's generally preferred over 'var' in modern JavaScript development.
 
+// another not-recommended way of initializing/declaring a variable is following:
+city = "ABC";
+// This works because, in non-strict mode, assigning to a variable without declaring it implicitly makes it a global variable, but it should be avoided as it can lead to unintended global variables and make code harder to manage.
