6c39f5b0fa21afc6358d2c902b7cc1f1e0ad42dd
ribalraza1 <ribalraza1@gmail.com>
2025-07-26 18:54:39 +0000

    Time and date in JS

--- 1_Basics/7_dateAndTime.js
+++ 1_Basics/7_dateAndTime.js
@@ -1,39 +1,81 @@
-/* Date and Time have been a pain point in JS. It means that it's a bit irritating to store and fetch
-Date and Time from Date object of JS.
-Date is a JS object which startes calculating time from January 1 1970 UTC (universal clock).
-*/
-// Defining Date
-const ourDate = new Date();
-console.log(ourDate, typeof ourDate); // It returns an object with a difficult to understand values
-console.log(ourDate.toString()); // conversion of Date to string returns readable values
+/**
+ * @fileoverview Date and Time in JS
+ */
+// Date is an object in javascript that encapsulates an integer number.
+// That integer number refers to milliseconds since 1 January 1970 mid-night
+// So date and time are calculated by default from 1 January 1970 mid-night (the epoch)
+// To get the current date and time
+const currentDate = new Date();
+console.log(currentDate);
+// new Date() returns current date but that's not readable
+// So we can try to covert the value of currentDate to string
+console.log(currentDate.toString()); // Output's date and time in UTC
+// The International Organization for Standardization format date and time is
+console.log(currentDate.toISOString());
+// Then to get date and time in local system's value
+console.log(currentDate.toLocaleString());
+// To get date only in local system value
+console.log(currentDate.toLocaleDateString());
+// To get time only in local system value
+console.log(currentDate.toUTCString()); // Coordinated Universal Time format
+// To get json version of date and time
+console.log(currentDate.toJSON()); // output is similar to toISOString()
 
-// Date Methods/Functions
-console.log(ourDate.toDateString()); // return Day of the week, Month, Date and year of system
-console.log(ourDate.toTimeString()); // returns system's time and standard in 24 hour format
-console.log(ourDate.toISOString()); // return date and time in ISO (Internatial Standard Org) format
-console.log(ourDate.toUTCString()); // returns system's date and time with respect to Universal time
-console.log(ourDate.toLocaleString()); // returns system's format of date of and time
-console.log(ourDate.toLocaleDateString()); // returns system's format of date
-console.log(ourDate.toLocaleTimeString()); // returns system's format of time
+// We can also define a specifc date
+const lastYearParty = new Date(2023, 10, 24);
+// Date object can take params in order
+// Year, monthIndex, date, hours, minutes, seconds, milliseconds
+// monthIndex starts from 0, so to save November which is 11th month, you can write 10
+console.log(lastYearParty.toLocaleString());
+// Another syntax for saving date
+const lastYearParty2 = new Date("11-24-2023"); // You can also write like "2023-11-24"
+console.log(lastYearParty2.toLocaleString());
+console.log(lastYearParty2.toDateString());
 
-/* there are some methodes that can get specific information from Date object like date, day, month,
-year, time, hours, minutes, seconds, milliseconds and etc*/
+// Date.now() gives current time in milliseconds since the epoch
+const now = Date.now();
+console.log(`Milliseconds since epoch now: ${now}`);
+
+const myCreatedDate = new Date("2024-09-13T12:30:00");
+console.log(`My created date: ${myCreatedDate.toDateString()}`);
+
+// milliseconds *since epoch*
+const timeFromMyDate = myCreatedDate.getTime();
+console.log(`Milliseconds since epoch at my date: ${timeFromMyDate}`);
+
+// difference = how much time has passed
+const diff = now - timeFromMyDate;
+console.log(`Milliseconds passed since my date: ${diff}`);
+
+// seconds passed
+const secondsPassed = Math.floor(diff / 1000);
+console.log(`Seconds passed since my date: ${secondsPassed}`);
+
+// minutes passed
+const minutesPassed = Math.floor(diff / (1000 * 60));
+console.log(`Minutes passed since my date: ${minutesPassed}`);
+
+// hours passed
+const hoursPassed = Math.floor(diff / (1000 * 60 * 60));
+console.log(`Hours passed since my date: ${hoursPassed}`);
+
+// convert to days
+const daysPassed = Math.floor(diff / (1000 * 60 * 60 * 24));
+console.log(`Days passed since my date: ${daysPassed}`);
+
+// Local string date time
+const newDate = new Date("07-26-2025");
+console.log(`Local string date time: ${newDate.toLocaleString()}`);
+// Some parameters for toLocaleString()
 console.log(
-  ourDate.getDate(),
-  ourDate.getDay(),
-  ourDate.getMonth(),
-  ourDate.getFullYear(),
-  ourDate.getTime().toLocaleString(),
-  ourDate.getHours(),
-  ourDate.getMinutes(),
-  ourDate.getSeconds()
+  `Local string date time: ${newDate.toLocaleString("default", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZone: "Asia/Karachi", // Time zone can be defined, It can be look up on Intl.supportedValuesOf('timeZone')
+  })}`
 );
-
-// declaring custom date, value of Jan is 0 and of Dec is 11
-const myDate = new Date(2023, 3, 25, 5, 33, 47);
-/* paramter postions are year, month, date, hour, minute, seconds and milliseconds. Once can only define year too*/
-console.log(myDate.toLocaleString());
-//there are some other ways to define date
-const myDate2 = new Date("2023-11-27"); // with strings and format YYYY-MM-DD
-const myDate3 = new Date("11-27-2023"); // with strings in format MM-DD-YYYY
-console.log(myDate2.toString(), myDate3.toString());
