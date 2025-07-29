238fa3fee6d2b407e5b23a8d1dd24de942f52c4d
ribalraza1 <ribalraza1@gmail.com>
2025-07-29 16:35:36 +0000

    Immediately Invoked Function Expression

--- 2_FunctionsAndScope/6_IIFE.js
+++ 2_FunctionsAndScope/6_IIFE.js
@@ -1,19 +1,30 @@
-/* IIFE (Immediately Invoked Function Expressions) 
-Sometimes we need to invoke/execute a function immediatly. There can be many reasons behind writing
-IIFE but following reasons are the common.
-1- We want to make a database connection or api call as soon as application starts, so we write
-   IFFE to achieve this.
-2- We don't want our scope to be polluted by Node.js or some other engine's global object.
+/**
+ * @fileoverview IIFE (Immediately Invoked Function Expression)
+ */
+/**
+ * 🧠 IIFE – Immediately Invoked Function Expression
+ *
+ * Sometimes we want to run a function immediately after defining it,
+ * without having to call it separately. For this, we use an IIFE.
+ *
+ * ✅ Common use cases for IIFE:
+ * 1. To execute setup logic right away (e.g., database connections, API calls on startup)
+ * 2. To isolate variables and avoid polluting the global scope (especially in older JavaScript or Node.js modules)
+ *
+ * 🔧 Syntax:
+ * An IIFE uses two sets of parentheses:
+ * - The first wraps the function: `(function() { ... })`
+ * - The second invokes it: `()`
+ * Why the extra parentheses?
+ * - JavaScript expects `function` declarations to have names.
+ * - Wrapping in `()` turns it into a function *expression*, which can be executed immediately.
+ */
 
-Two write IFFE, we write two sets of paranthesis, like this ()(), in first paranthesis we write our
-function and the second paranthesis denotes immediately invoked execution of the
-function declared in first set of paranthesis
-*/
-
-// IFFE with regular function
-(function greet1() {
-  console.log("Hello Welcome");
-})(); // It is necessary to put semicolon ';' after IFFE because IFFE doesnot know where to end
+// IIFE with traditional function definition
+(function () {
+  console.log("Hello! Welcome");
+})();
+// It is necessary to put semicolon ';' after IFFE because IFFE doesnot know where to end
 
 // IFFE with Arrow function
 (() => {
