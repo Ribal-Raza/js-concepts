d5e723718914eae9c3262b152b11a008b58379c7
ribalraza1 <ribalraza1@gmail.com>
2025-08-03 16:59:46 +0000

    Project 3: Digital clock

--- 5_Beginner_DOM_Projects/3_Clock/script.js
+++ 5_Beginner_DOM_Projects/3_Clock/script.js
@@ -1,18 +1,8 @@
-const clock = document.querySelector("#clock");
-
-/* 
-We have targeted the clock section. Now we have to show the time. For this purpose, we create
-a new "date" varibale and assign the Date() object to it because we know Date() object has information like
-date and time of the system. 
-we can acess the system's time by date.toLocaleTimeString() and set it to innerHTMl of the clock.
-But it will only show the time when the page was loaded. To keep updating and changing the innerHTML
-of our clock area, 
-we can use setInterval() methode. It take two arguments
-1- A callback function where we write our logic
-2- A specific value of time in milliseconds after which we want to re-run our code of callback function
-*/
-
+// Select the clock element from the DOM
+const clock = document.getElementById("clock");
+// setInterval() repeatedly calls a function or executes a code snippet,
+// with a fixed time delay between each call.
 setInterval(() => {
-  let date = new Date();
+  const date = new Date();
   clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
