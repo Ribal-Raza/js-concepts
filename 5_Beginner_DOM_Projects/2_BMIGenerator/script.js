262700e56706f6eb0e7371c0391eb2fb5f5e6a99
ribalraza1 <ribalraza1@gmail.com>
2025-08-03 16:43:35 +0000

    Project 2: BMI Calculator

--- 5_Beginner_DOM_Projects/2_BMIGenerator/script.js
+++ 5_Beginner_DOM_Projects/2_BMIGenerator/script.js
@@ -1,38 +1,25 @@
 const form = document.querySelector("form");
-
-/* As we have accessed the form, now we need to understand one thing, whenever the user will click 
-"calculate" button, the values in the form will be submitted. These values automatically go to server
-or url. In this project as we are focusing on calculating BMI on page, not through server. So we have to
-stop this default behaviour of sending values to server*/
-
+const results = document.querySelector("#results");
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // prevents default behaviour of form
-
-  // now we have to grab values of height and weight entered in the form inputs when form is submitted
-
-  /* querySelector() returns the whole element, we have to extract it's value with ".value" property
-  but .value returns value in string, so we have to convert it in integer with parseInt() */
+  event.preventDefault();
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
 
-  // lets also grab results element
-  const results = document.querySelector("#results");
-
-  // apply some checks on input values
   if (height === "" || height < 0 || isNaN(height)) {
-    results.innerHTML = "Please enter a valid height";
+    results.innerHTML = `Please Enter a valid Height ${height}`;
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
-    results.innerHTML = "Please enter a valid weight";
+    results.innerHTML = "Please Enter a valid Weight";
   } else {
-    const BMI = weight / ((height * height) / 10000).toFixed(2);
-    let remarks;
-    if (BMI < 18.6) {
-      remarks = "You are under weight";
-    } else if (BMI >= 18.6 && BMI <= 24.9) {
-      remarks = "Your BMI is normal";
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    if (bmi < 18.6) {
+      results.innerHTML = `<span>${bmi}: Underweight</span>`;
+      results.style.color = "#cdab00";
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      results.innerHTML = `<span>${bmi}: Normal</span>`;
+      results.style.color = "#008000";
     } else {
-      remarks = "You are over weight";
+      results.innerHTML = `<span>${bmi}: Overweight</span>`;
+      results.style.color = "#ff0000";
     }
-    results.innerHTML = `<span>You BMI is: ${BMI} </span> <span>${remarks}</span>`;
   }
 });
