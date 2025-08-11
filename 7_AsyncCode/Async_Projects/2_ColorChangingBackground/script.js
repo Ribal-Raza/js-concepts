112784b1e6850aeffc53fe8eb37e390d0ac872f7
ribalraza1 <ribalraza1@gmail.com>
2025-08-11 04:42:03 +0000

    Background Color changing

--- 7_AsyncCode/Async_Projects/2_ColorChangingBackground/script.js
+++ 7_AsyncCode/Async_Projects/2_ColorChangingBackground/script.js
@@ -1,33 +1,27 @@
-const btnStart = document.querySelector("#start");
-const btnStop = document.querySelector("#stop");
-const body = document.querySelector("body");
-
-// generation of random colors
-const randomColors = () => {
-  const hex = "0123456789ABCDEF";
+// Generate Random Hex Color code
+// Hex Code: #RRGGBB
+// RR -> Red, GG -> Green, BB -> Blue
+// Each pair can have values from 00 to FF where each item has values (0,..,9,A,B,C,D,E,F)
+const randomColor = function () {
+  const hexString = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
-    color += hex[Math.floor(Math.random() * 16)];
+    let randomPosition = Math.floor(Math.random() * 16);
+    color += hexString[randomPosition];
   }
   return color;
 };
-let interval;
-const startColorChange = () => {
-  if (!interval) {
-    interval = setInterval(colorChange, 1000);
+console.log(randomColor());
+let intervalId;
+document.querySelector("#start").addEventListener("click", (e) => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      document.querySelector("body").style.transition = "ease-out 1s";
+      document.querySelector("body").style.backgroundColor = randomColor();
+    }, 1000);
   }
-  function colorChange() {
-    body.style.background = randomColors();
-    console.log("color changing started");
-  }
-};
-
-btnStart.addEventListener("click", (e) => {
-  startColorChange();
 });
-
-btnStop.addEventListener("click", (e) => {
-  clearInterval(interval);
-  interval = null;
-  console.log("color changing stoped");
+document.querySelector("#stop").addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
 });
