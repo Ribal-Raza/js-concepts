1af142127f8711404d9a1570a6e52cab6865a931
ribalraza1 <ribalraza1@gmail.com>
2025-08-30 18:35:38 +0000

    object related concepts

---
+++ 9_OOP/8_object-concepts.js
@@ -0,0 +1,4 @@
+// Question: In JS, Math.PI has value 3.141592653589793, can we change it or not?
+// If yes, then how?
+// Answer to this question gives us more indepth knowledge about objects in JS.
+// Let's explore it.
